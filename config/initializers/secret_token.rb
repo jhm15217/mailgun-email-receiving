54955759d5d5d086ea2c6e015f51e8149a9bea37
Michael Villena <mvillena3@gatech.edu>
2012-07-03 23:00:09 -0400

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmailReceiver::Application.config.secret_token = '6e50a7a6a1748b88ce2cac234d10404290f6194b991c309145fa9a1914683e1e0ac92c610579c968c343259355968639ff2ca4420dd3d9eba3fc1293ef72c459'
